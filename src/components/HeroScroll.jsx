4f5b5fcf585a9fc765a037af6867e179b3041d4f
yossisossi445-web <yossisossi445@gmail.com>
2026-06-25 23:35:51 +0300

    Update HeroScroll.jsx

--- src/components/HeroScroll.jsx
+++ src/components/HeroScroll.jsx
@@ -166,7 +166,7 @@ const HeroScroll = () => {
           
           <div ref={text1Ref} style={{ 
            position: 'absolute', 
-           top: '50vh', 
+           top: '45vh', 
            width: '100%',
            textAlign: 'center',
            padding: '0 20px',
